5b4f7a865a5752affc846f91a74ecb26a6267f73
Lenny Chen <lenny83920@gmail.com>
2018-01-22 17:23:22 +0800

    Add files via upload

--- add.aspx.cs
+++ add.aspx.cs
@@ -54,30 +54,40 @@ public partial class Test_List_add : Page
     {
         SqlConnection conn = new SqlConnection("Data Source=400-36042696\\SQLEXPRESS;Initial Catalog=testDB;" +
                             "Integrated Security=true;");
+        conn.Open();
         SqlCommand cmd = null;
-        try
-        {
-            conn.Open();
-            cmd = new SqlCommand("Insert Into Teacher (TID,TName,TPhone,TSubject,TSex) values " +
-                "(@TID,@TName,@TPhone,@TSubject,@TSex)", conn);
-            cmd.Parameters.Add("@TID", SqlDbType.Int).Value = TID.Text;
-            cmd.Parameters.Add("@TName", SqlDbType.NChar, 10).Value = TName.Text;
-            cmd.Parameters.Add("@TPhone", SqlDbType.NVarChar, 20).Value = TPhone.Text;
-            cmd.Parameters.Add("@TSubject", SqlDbType.NChar, 10).Value = TSubject.Text;
-            cmd.Parameters.Add("@TSex", SqlDbType.NChar, 10).Value = TSex.Text;
-            cmd.ExecuteNonQuery();
-        }
-        catch (Exception ex)
-        {
-            Response.Write(ex.ToString());
-        }
-        finally
-        {
-            cmd.Dispose();
-            conn.Close();
-            conn.Dispose();
+        SqlCommand search = null;
 
-            Response.Redirect("Teacher_List.aspx");
+        search = new SqlCommand("Select * From Teacher Where TID=@TID;", conn);
+        search.Parameters.Add("@TID", SqlDbType.Int).Value = TID.Text;
+
+        if (search.ExecuteScalar() != null) {
+            Response.Write("<script>alert('Teacher is exisit!')</script>");
+        }
+        else {
+            try
+            {
+                cmd = new SqlCommand("Insert Into Teacher (TID,TName,TPhone,TSubject,TSex) values " +
+                    "(@TID,@TName,@TPhone,@TSubject,@TSex)", conn);
+                cmd.Parameters.Add("@TID", SqlDbType.Int).Value = TID.Text;
+                cmd.Parameters.Add("@TName", SqlDbType.NChar, 10).Value = TName.Text;
+                cmd.Parameters.Add("@TPhone", SqlDbType.NVarChar, 20).Value = TPhone.Text;
+                cmd.Parameters.Add("@TSubject", SqlDbType.NChar, 10).Value = TSubject.Text;
+                cmd.Parameters.Add("@TSex", SqlDbType.NChar, 10).Value = TSex.Text;
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.ToString());
+            }
+            finally
+            {
+                cmd.Dispose();
+                conn.Close();
+                conn.Dispose();
+
+                Response.Redirect("Teacher_List.aspx");
+            }
         }
     }
 }
